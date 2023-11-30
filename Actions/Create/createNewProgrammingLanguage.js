0eccb524102ccff8cd0c64c080505d80e3ffb64e
„michellesaric“ <msaric03@fesb.hr>
2023-11-30 17:18:57 +0100

    Added create function for programming languages

--- Actions/Create/createNewProgrammingLanguage.js
+++ Actions/Create/createNewProgrammingLanguage.js
@@ -1,1 +1,20 @@
-const createNewProgrammingLanguage = () => {};
+const createNewProgrammingLanguage = () => {
+  const length = programmingLanguages.length;
+
+  const programmingLanguage = prompt(
+    "Please enter the name of the new programming language:"
+  );
+  if (!programmingLanguage.trim()) {
+    alert("Your input vas invalid, you will be returned back.");
+    actionsWithProgrammingLanguages();
+    return;
+  }
+  const newLanguage = {
+    id: programmingLanguages[length - 1].id + 1,
+    name: programmingLanguage,
+  };
+
+  programmingLanguages.push(newLanguage);
+  mainMenu();
+  return;
+};
