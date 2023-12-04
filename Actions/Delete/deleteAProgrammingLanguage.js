773a659d7099372c18849a3444149ebab9461301
„michellesaric“ <msaric03@fesb.hr>
2023-12-04 15:12:11 +0100

    Added a delete function for programming languages

--- Actions/Delete/deleteAProgrammingLanguage.js
+++ Actions/Delete/deleteAProgrammingLanguage.js
@@ -1,1 +1,29 @@
-const deleteAProgrammingLanguage = () => {};
+const deleteAProgrammingLanguage = () => {
+  const languageId = prompt(
+    `Here is a list of all programming languages':\n\n${programmingLanguages
+      .map((language, index) => `${index + 1} - ${language.name}`)
+      .join("\n")}\nChoose the id of the language you want to delete:`
+  );
+
+  if (
+    isNaN(languageId) ||
+    languageId < 1 ||
+    languageId > programmingLanguages.length
+  ) {
+    alert("The input you put in is invalid");
+    actionsWithProgrammingLanguages();
+  }
+
+  const deletedLanguageIndex = programmingLanguages.findIndex(
+    (programmingLanguage) => programmingLanguage.id == languageId
+  );
+  programmingLanguages.splice(deletedLanguageIndex, 1);
+  alert("Programming language deleted successfully.");
+
+  for (let i = deletedLanguageIndex; i < programmingLanguages.length; i++) {
+    programmingLanguages[i].id = i + 1;
+  }
+
+  actionsWithProgrammingLanguages();
+  return;
+};
